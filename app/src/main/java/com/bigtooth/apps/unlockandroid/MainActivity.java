c8a821eed4090f6eaaf84541517341cf9b29eb39
Octane Development <rockoutboy@yahoo.com>
2016-07-19 12:07:02 -0400

    Add files via upload

---
+++ app/src/main/java/com/bigtooth/apps/unlockandroid/MainActivity.java
@@ -0,0 +1,104 @@
+package com.bigtooth.apps.unlockandroid;
+
+import android.app.*;
+import android.os.*;
+import android.support.v7.app.AppCompatActivity;
+import android.view.*;
+import android.widget.*;
+import android.content.Intent;
+import android.view.View;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity
+{
+	InterstitialAd mInterstitialAd;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+	{
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+		MobileAds.initialize(getApplicationContext(), "ca-app-pub-5164171001589422/2057833399");
+
+		AdView mAdView = (AdView) findViewById(R.id.adView);
+		AdRequest adRequest = new AdRequest.Builder().build();
+		mAdView.loadAd(adRequest);
+
+		mAdView.setAdListener(new AdListener() {
+			@Override
+			public void onAdOpened() {
+				// Save app state before going to the ad overlay.
+			}
+		});
+
+		mInterstitialAd = new InterstitialAd(this);
+		mInterstitialAd.setAdUnitId("ca-app-pub-5164171001589422/3534566592");
+
+		mInterstitialAd.setAdListener(new AdListener() {
+			@Override
+			public void onAdClosed() {
+				requestNewInterstitial();
+			}
+		});
+
+		requestNewInterstitial();
+	}
+
+	private void requestNewInterstitial() {
+		AdRequest adRequest = new AdRequest.Builder()
+				.addTestDevice("5C6759521D465119182182A234FF5CE3")
+				.build();
+
+		mInterstitialAd.loadAd(adRequest);
+	}
+
+	public void pre(View view) {
+		if (mInterstitialAd.isLoaded()) {
+			mInterstitialAd.show();
+			mInterstitialAd.setAdListener(new AdListener() {
+				public void onAdClosed() {
+					Intent intent = new Intent(MainActivity.this, Pre.class);
+					startActivity(intent);
+				}
+			});
+		} else {
+			Intent intent = new Intent(this, Pre.class);
+			startActivity(intent);
+		}
+	}
+	public void how(View view) {
+		if (mInterstitialAd.isLoaded()) {
+			mInterstitialAd.show();
+			mInterstitialAd.setAdListener(new AdListener() {
+				public void onAdClosed() {
+					Intent intent = new Intent(MainActivity.this, How.class);
+					startActivity(intent);
+				}
+			});
+		} else {
+			Intent intent = new Intent(this, How.class);
+			startActivity(intent);
+		}
+	}
+	public void about(View view) {
+		if (mInterstitialAd.isLoaded()) {
+			mInterstitialAd.show();
+			mInterstitialAd.setAdListener(new AdListener() {
+				public void onAdClosed() {
+					Intent intent = new Intent(MainActivity.this, About.class);
+					startActivity(intent);
+				}
+			});
+		} else {
+			Intent intent = new Intent(this, About.class);
+			startActivity(intent);
+		}
+	}
+}
---
+++ app/src/main/java/com/bigtooth/apps/unlockandroid/Pre.java
@@ -0,0 +1,61 @@
+package com.bigtooth.apps.unlockandroid;
+
+import android.app.*;
+import android.os.*;
+import android.support.v7.app.AppCompatActivity;
+import android.view.*;
+import android.widget.*;
+import android.content.Intent;
+import android.view.View;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
+
+public class Pre extends AppCompatActivity
+{
+    InterstitialAd mInterstitialAd;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+	{
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pre);
+
+        MobileAds.initialize(getApplicationContext(), "ca-app-pub-5164171001589422/2057833399");
+
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        mAdView.setAdListener(new AdListener() {
+            @Override
+            public void onAdOpened() {
+                // Save app state before going to the ad overlay.
+            }
+        });
+
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-5164171001589422/3534566592");
+
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                requestNewInterstitial();
+            }
+        });
+
+        requestNewInterstitial();
+    }
+
+    private void requestNewInterstitial() {
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice("5C6759521D465119182182A234FF5CE3")
+                .build();
+
+        mInterstitialAd.loadAd(adRequest);
+    }
+}
